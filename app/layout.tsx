c64854bd6233ec6959dce5fa9917496d21124b36
shshwtsuthar <shshwtsuthar@gmail.com>
2025-01-14 23:37:42 +1100

    Dashboard Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +29,22 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,85 @@
+import { Calendar, Home, Inbox, Search, Settings, ArrowLeftRight, ChartScatter, Flag, ScrollText, CircleGauge } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import { NavUser } from "./nav-user";
+
+const data = {
+  user: {
+    name: "Shashwat Suthar",
+    email: "shshwtsuthar@gmail.com",
+    avatar: "UP_STROKE_LOGO.jpg",
+  }
+}
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: CircleGauge,
+  },
+  {
+    title: "Accounts",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Transactions",
+    url: "#",
+    icon: ArrowLeftRight,
+  },
+  {
+    title: "Spending Insights",
+    url: "#",
+    icon: ChartScatter,
+  },
+  {
+    title: "Budgets",
+    url: "#",
+    icon: Flag,
+  },
+  {
+    title: "Reports",
+    url: "#",
+    icon: ScrollText,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user}/>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
